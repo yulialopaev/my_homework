a7b8c6dd8a6544d0f54e4c9396cb3f00e03b2eaa
Yulia Lopaev <yulia.lopaev@gmail.com>
2026-04-30 11:10:22 +0300

    feat: homework 10 modal_numbers

---
+++ Frontend 65-35/hw10/hw10_modal_and_numbers.js
@@ -0,0 +1,50 @@
+let my_number = prompt("Enter the number and I'll return it in reverse:")
+if (my_number === null) {
+    alert("You cancelled the input.")
+} else if (isNaN(my_number) || my_number.trim() === "") {
+    alert("Please enter a valid number.")
+} else {
+    alert(`Reverse of the number ${my_number}: ${reverse(my_number)}.`)
+    alert(`Sum of digits: ${sumDigits(my_number)}.`)
+}
+
+function reverse(number) {
+    let abs_number = Math.abs(number)
+    let num_integer = Math.trunc(abs_number)
+    let num_string = num_integer.toString()
+    let num_length = num_string.length
+
+
+    let new_string = ""
+    while (num_length > 0) {
+        new_string += num_string.charAt(num_length - 1)
+        num_length = num_length - 1
+    }
+
+    let result_1
+    if (number >= 0) {
+        result_1 = `${Number(new_string)}`
+    } else {
+        result_1 = `${-Number(new_string)}`
+    }
+
+    return result_1
+
+}
+
+
+function sumDigits(number) {
+    let abs_number = Math.abs(number)
+    let num_integer = Math.trunc(abs_number)
+    let num_string = num_integer.toString()
+    let num_array = Array.from(num_string)
+
+    let result_2 = 0
+
+    for (let digit of num_array) {
+        result_2 += Number(digit)
+    }
+
+    return result_2
+}
+
